a9a90ea7d66e4ffa84d8c27c5d88a180c89859fb
vanznero <nurulfernandasuhada@gmail.com>
2024-05-25 16:46:34 +0700

    addsore

---
+++ myrestapi-master/server.js
@@ -0,0 +1,16 @@
+require("./settings");
+const http = require("http");
+const app = require("./index");
+const PORTHOST = port || 8080;
+
+http.createServer(app).listen(PORTHOST, () => {
+    console.log(`
+    ╔══╗╔╗ ♡ ♡ ♡
+    ╚╗╔╝║║╔═╦╦╦╔╗
+    ╔╝╚╗║╚╣║║║║╔╣
+    ╚══╝╚═╩═╩═╩═╝  
+			                          @Lana|Dev
+								 
+Server running on http://localhost:` + PORTHOST)
+console.log(`Hello ${creator}`)
+})
